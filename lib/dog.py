69e97c180338cd30a3f7bbb897a9fd5bc7d1801e
sidee <said.issack@student.moringaschool.com>
2023-08-21 23:15:14 +0300

    completed assighnment

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,13 @@
 
 class Dog:
     pass
+    def __init__(self, name, breed= 'Mutt'):
+        self.name = name
+        self.breed = breed
+
+
+
+
+doggo = Dog('Rufus')
+print(doggo.name)
+print(doggo.breed)
--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,9 @@
 
 class Person:
     pass
+
+    def __init__(self, name):
+        self.name = name
+
+moses = Person('saidfaris')
+print(moses.name)
